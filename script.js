8bcfeffadd12ff0b257fbfc60cb5adc6dc64ae22
Olaniyi Osoba <stpaulosoba@gmail.com>
2025-10-26 20:59:20 +0000

    started javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+const Accounts = localStorage;
+
+// Login Section 
+// A button event listener will be added here
+const username = document.getElementById(Username.value);
+const password = document.getElementById(Password.value);
+
+if () {
+
+}
