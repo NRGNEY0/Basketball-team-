9252ebcfffb931a942adf4ec151a61b0994f0612
Olaniyi Osoba <stpaulosoba@gmail.com>
2026-01-04 15:38:00 +0000

    Have implemented all the js code, struggling with inteactivity of the website tho

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-const Accounts = localStorage;
 
-// Login Section 
-// A button event listener will be added here
-const username = document.getElementById(Username.value);
-const password = document.getElementById(Password.value);
+console.log("js is running");
+// create account 
+const form = document.getElementById('register');
+form.addEventListener('submit', (event) => {
+    event.preventDefault; // Prevents the page from refreshing 
+    const username = document.getElementById('CUsername').value;
+    const password = document.getElementById('CPassword').value;
 
-if () {
 
-}
+    if (!username || !password) { // checks to see if both username and password fields are filled 
+        alert('Pleadse fill in all fields');
+        return;
+    }
+
+    //checks to see if the username exists 
+
+    if (localStorage.getItem(username)){
+        alert("Username already exists");
+        return;
+    }
+
+    // Stores the user 
+    localStorage.setItem(username,password);
+    alert("Account created successfully");
+})
+
+//Login
+document.getElementById("btnLogin").addEventListener('click', () => {
+    const Username = document.getElementById('LUsername').value;
+    const Password = document.getElementById("Lpassword").value;
+
+    // check if user exeists
+    const storedPassword = localStorage.getItem(Username);
+
+    if (!storedPassword) {
+        message.textContent = 'Account not found. Please create one';
+    } else if (storedPassword === Password) {
+        message.style.color = 'green';
+        message.textContent = "Login Successful";
+    } else {
+        message.textContent= 'incorrect password';
+    }
+});
