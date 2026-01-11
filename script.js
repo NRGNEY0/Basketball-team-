2eaf6de6359bd303f2e644068307230a8bf826b7
Olaniyi Osoba <stpaulosoba@gmail.com>
2026-01-11 11:10:58 +0000

    Have fixed all the bugs and have added messages for form validation instead of alerts, next will focus on implementing email services

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const message = document.getElementById("message");
+
+//Function to show  messages 
+function ShowMessage(text, type){ // text for the text it will show the user and type for the type of message either error or success 
+    message.textContent = text; // Chamges the text in the div 
+    message.className = `message ${type}` // changes the class of the div 
+    message.style.display = "block"; // makesthe message show 
+}
 
 // create account 
 const registerForm = document.getElementById('register');
@@ -9,23 +17,24 @@ registerForm.addEventListener('submit', (event) => {
 
 
     if (username === ''|| password === '') { // checks to see if both username and password fields are filled 
-        alert('Please fill in all fields');
+        ShowMessage("Please fill in all fields", "error");
         return;
     }
 
     //checks to see if the username exists 
 
     if (localStorage.getItem(username)){
-        alert("Username already exists");
+        ShowMessage("Username already exists", "error");
         return;
     }
 
     // Stores the user 
     localStorage.setItem(username,password);
-    alert("Account created successfully");
+    ShowMessage("Account created successfully", "success");
+
+
     
-    username = " ";
-    password = " ";
+    event.target.reset() // clears out all input fields 
 });
 }
 
@@ -39,17 +48,23 @@ if (loginForm){
 
     // Making sure that all fields arent left blank 
      if (username === '' || password === '') {
-        alert('Please fill in all fields')
+        ShowMessage("Please fill in all fields", "error")
+        return;
      }
     // check if user exeists
     const storedPassword = localStorage.getItem(username);
 
     if (!storedPassword) {
-        alert("Account not found, please create one");
+        ShowMessage("Account not found, please create one", "error");
+        return;
     } else if (storedPassword === password) {
-        alert("Login successful")
+        ShowMessage("Login successful", "success")
     } else {
-        alert("incorrect password");
+        ShowMessage("incorrect password", "error");
     }
+
+    event.target.reset() //clears all the fields
 });
 }
+
+
