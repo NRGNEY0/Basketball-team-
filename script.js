1750c333a64ab2e6bbf4cc79a924ef6cd3afeb4a
Olaniyi Osoba <stpaulosoba@gmail.com>
2026-01-25 17:32:50 +0000

    Fix: Remove event prevent derfault in the bookign script block

--- script.js
+++ script.js
@@ -73,10 +73,8 @@ if (loginForm){
 const Booking = document.getElementById("Booking");
 if (Booking){ 
     Booking.addEventListener("submit", (event) =>{
-        event.preventDefault()
         ShowMessage("Booking submitted", "green") // if the contact variable isnt null it will submit the form(done in the html action) and show a confirmation message
-        
-    })
+    });
 }
 
 
